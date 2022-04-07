050479b0cb1a772ace8e8ca09bccbbccb2686410
TrevisTrovis <trevorpearce94@yahoo.com>
2022-04-07 11:19:11 -0400

    first_java

---
+++ Week1/first_java/MyInfo.java
@@ -0,0 +1,9 @@
+public class MyInfo {
+
+    public static void main(String[] args){
+        System.out.println("My name is Trevor Ottley");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Geneva, NY");
+    }
+    
+}
