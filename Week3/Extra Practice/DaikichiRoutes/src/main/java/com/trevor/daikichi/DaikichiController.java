bcc31795439a996e110003bf89e0a63574484bef
TrevisTrovis <trevorpearce94@yahoo.com>
2022-04-20 16:11:27 -0400

    Daikichi Routes Project

--- Week3/Extra Practice/DaikichiRoutes/src/main/java/com/trevor/daikichi/DaikichiController.java
+++ Week3/Extra Practice/DaikichiRoutes/src/main/java/com/trevor/daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.trevor.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,21 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an oppurtunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("travel/{city}")
+	public String city(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	
+	@RequestMapping("lotto/{luckyNum}")
+		public String lotto(@PathVariable("luckyNum") int luckyNum) {
+			if(luckyNum %2 == 0) {
+				return "You will take a grand journey in the near future, but be wary of tempting offers.";
+			}
+			else {
+				return "You have enjoyed the fruits of your labor, but now is a good time to spend time with friends and family";
+			}
 	}
 }	
 
--- Week3/Extra Practice/helloWorld/src/main/java/com/example/demo/HomeController.java
+++ Week3/Extra Practice/helloWorld/src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,11 @@ public class HomeController {
 	@RequestMapping("/greeting")
 	public String greeting() {
 		return "Hi, Trevor!";
+	}
+	
+	@RequestMapping("/m/{track}/{module}/{lesson}")
+	public String newLesson(@PathVariable("track")String track, @PathVariable("module")String module, @PathVariable("lesson")String lesson) {
+		return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
 	}
 
 }
